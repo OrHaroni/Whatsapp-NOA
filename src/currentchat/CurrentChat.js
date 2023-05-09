cc45779d92dc7a21295ab8058e7e6f8c40d0fa44
AdarKatz <110082803+AdarKatz@users.noreply.github.com>
2023-05-09 14:52:06 +0300

    changes

--- src/currentchat/CurrentChat.js
+++ src/currentchat/CurrentChat.js
@@ -1,8 +1,8 @@
 function CurrentChat(props) {
     return (
         <div id="current-chat-info">
-            <img src={props.groupPic} className="rounded-circle profile-pic-in-div" />
-            <span id="chat-name">{props.groupName}</span>
+                <img src={props.groupPic} className="rounded-circle profile-pic-in-div" />
+             <span id="chat-name">{props.groupName}</span>
         </div>
     );
 }
