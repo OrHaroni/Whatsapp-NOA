4e4d33ee3a833e1ac8681df5a542f17435182a51
AdarKatz <110082803+AdarKatz@users.noreply.github.com>
2023-05-16 10:49:33 +0300

    Modal style

--- src/chatPreview/ChatPreview.js
+++ src/chatPreview/ChatPreview.js
@@ -4,23 +4,38 @@ import '../noa.css';
 //Returning the HTML code of a Chat Preview
 function ChatPreview(props) {
   const id = "chat-" + props.id;
-  const now = new Date();
-  const currentDate = now.toLocaleDateString('en-GB');
-  const currentHour = now.getHours();
-  const currentMinute = now.getMinutes();
-    return(
-        <li onClick={props.onClick} onMouseEnter={props.in} onMouseLeave={props.out} className="chat-tag" id={id}>
-            <div>
-              <img src={props.img} className="rounded-circle chat-profile-pic" />
-                </div>
-                <div>
-                  <span className="chat-tag-label">{props.name}</span>
-                </div>
-                <div>
-                  <span className="chat-tag-label">{currentDate},{currentHour}:{currentMinute}</span>
-                </div>
-              </li>
-    );
+  
+  if (props.messageList.length === 0) {
+    return (
+      <li onClick={props.onClick} onMouseEnter={props.in} onMouseLeave={props.out} className="chat-tag" id={id}>
+        <div>
+          <img src={props.img} className="rounded-circle chat-profile-pic" />
+        </div>
+        <div>
+          <span className="chat-tag-label">{props.name}</span>
+        </div>
+      </li>
+    )
+
+
+  }
+  else {
+    return (
+      <li onClick={props.onClick} onMouseEnter={props.in} onMouseLeave={props.out} className="chat-tag" id={id}>
+        <div>
+          <img src={props.img} className="rounded-circle chat-profile-pic" />
+        </div>
+        <div>
+          <span className="chat-tag-label">{props.name}</span>
+        </div>
+        <div>
+          <span className="chat-tag-label"></span>
+        </div>
+      </li>
+    )
+
+  }
+
 }
 
 export default ChatPreview;
